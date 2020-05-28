5a6994dc992ca7307cd328bc47c3b45b55b086e5
taannu <tavo.annus@gmail.com>
2020-05-28 14:17:27 +0300

    Initial setup [wip]

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by TavoAnnus on 28/05/2020.
+//
+
+#include <iostream>
+
+#include <opencv2/opencv.hpp>
+
+int main(int argc, char** argv) {
+    std::cout << "Hey" << std::endl;
+    return 0;
+}
+
