e47b390dc92b5f72dd4e2023d9f27af6a3d6f21c
tavo <tavo.annus@gmail.com>
2020-06-19 16:01:43 +0300

    Better detect function

--- src/cam/detector/facedetector.cpp
+++ src/cam/detector/facedetector.cpp
@@ -6,7 +6,9 @@
 
 #include <utility>
 
-FaceDetector::FaceDetector() {
+FaceDetector::FaceDetector(double frameHeight, double frameWidth) :
+        frameHeight(frameHeight),
+        frameWidth(frameWidth) {
     if (!frontalFaceClassifier.load("./data/haarcascades/haarcascade_frontalface_default.xml")) {
         std::cout << "Unable to load classifier data for frontal face!" << std::endl;
     }
@@ -15,10 +17,70 @@ FaceDetector::FaceDetector() {
     }
 }
 
-std::vector<Face> FaceDetector::detectFaces() {
-    return std::vector<Face>();
+std::vector<Face> FaceDetector::detectFaces(const cv::Mat &frame) {
+    std::vector<Face> faces;
+
+    std::vector<cv::Rect> frontalFaces, profileFaces, flippedProfileFaces;
+    cv::Mat gray, smallImg, flippedSmallImg;
+
+    // Convert to grayscale
+    cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
+
+    // Resize
+    double fx = faceRecognitionFrameWidth / frameWidth;
+    double fy = faceRecognitionFrameHeight / frameHeight;
+    cv::resize(gray, smallImg, cv::Size(), fx, fy, cv::INTER_LINEAR);
+    cv::equalizeHist(smallImg, smallImg);
+    cv::flip(smallImg, flippedSmallImg, 1);
+
+    // Detect faces of different sizes using cascade classifier
+    frontalFaceClassifier.detectMultiScale(
+            smallImg,
+            frontalFaces,
+            1.1,
+            2,
+            (uint) 0 | cv::CASCADE_SCALE_IMAGE,
+            cv::Size(10, 10)
+    );
+
+    profileFaceClassifier.detectMultiScale(
+            smallImg,
+            profileFaces,
+            1.1,
+            2,
+            (uint) 0 | cv::CASCADE_SCALE_IMAGE,
+            cv::Size(10, 10)
+    );
+
+    profileFaceClassifier.detectMultiScale(
+            flippedSmallImg,
+            flippedProfileFaces,
+            1.1,
+            2, (uint) 0 | cv::CASCADE_SCALE_IMAGE,
+            cv::Size(10, 10)
+    );
+
+    for (const auto &r : frontalFaces) {
+        faces.emplace_back(r, true);
+    }
+
+    for (const auto &r: flippedProfileFaces) {
+        faces.emplace_back(cv::Rect(faceRecognitionFrameWidth - r.x - r.width, r.y, r.width, r.height), false);
+    }
+
+    for (const auto &r : profileFaces) {
+        faces.emplace_back(r, false);
+    }
+
+    frameBuffer[frameBufferIndexPointer++] = Frame(faces);
+
+    // TODO: Merge rectangles, some more statistics etc.
+
+    return faces;
 }
 
 FaceDetector::~FaceDetector() = default;
 
 Face::Face(cv::Rect bounds, const bool isFrontal) : bounds(std::move(bounds)), isFrontal(isFrontal) {}
+
+Frame::Frame(std::vector<Face> faces) : faces(std::move(faces)) {}
--- src/cam/detector/facedetector.h
+++ src/cam/detector/facedetector.h
@@ -16,20 +16,34 @@ struct Face {
     Face(cv::Rect bounds, bool isFrontal);
 };
 
+struct Frame {
+    std::vector<Face> faces;
+
+    Frame(std::vector<Face> faces);
+};
+
 
 class FaceDetector {
 public:
 
-    std::vector<Face> detectFaces();
+    std::vector<Face> detectFaces(const cv::Mat& frame);
 
-    FaceDetector();
+    FaceDetector(double frameHeight, double frameWidth);
 
     ~FaceDetector();
 
 private:
     cv::CascadeClassifier profileFaceClassifier;
     cv::CascadeClassifier frontalFaceClassifier;
-    Face frameBuffer[FRAME_BUFFER_LENGTH];
+    Frame frameBuffer[FRAME_BUFFER_LENGTH];
+
+    uint frameBufferIndexPointer = 0;
+
+    int faceRecognitionFrameHeight = 54 * 2;
+    int faceRecognitionFrameWidth = 96 * 2;
+
+    double frameHeight = 720;
+    double frameWidth = 1280;
 };
 
 
