83d16e64f0658cc6f174ba86f74a687969f9ffb7
tavo <tavo.annus@gmail.com>
2020-06-20 22:19:36 +0300

    Kinda getting there

--- src/cam/graphics/graphics.cpp
+++ src/cam/graphics/graphics.cpp
@@ -4,9 +4,11 @@
 
 #include "graphics.h"
 
+#define GAUSSIAN_STEP 5
+
 cv::Mat Graphics::grayscaleBackground(const cv::Mat &img, const std::vector<cv::Rect>& highlights) {
     cv::Mat grayscale;
-    cv::Mat mask = cv::Mat::zeros(img.rows, img.cols, CV_8UC1);
+    cv::Mat mask = cv::Mat::zeros(img.rows / GAUSSIAN_STEP, img.cols / GAUSSIAN_STEP, CV_8UC1);
     cv::cvtColor(img, grayscale, cv::COLOR_BGR2GRAY);
     cv::cvtColor(grayscale, grayscale, cv::COLOR_GRAY2BGR);
 
@@ -15,17 +17,18 @@ cv::Mat Graphics::grayscaleBackground(const cv::Mat &img, const std::vector<cv::
     for (const cv::Rect& rect : highlights) {
         cv::rectangle(
                 mask,
-                cv::Point(rect.x, rect.y),
-                cv::Point(rect.x + rect.width - 1, rect.y + rect.height - 1),
+                cv::Point(rect.x / GAUSSIAN_STEP, rect.y / GAUSSIAN_STEP),
+                cv::Point((rect.x + rect.width) / GAUSSIAN_STEP - 1, (rect.y + rect.height) / GAUSSIAN_STEP - 1),
                 cv::Scalar(255, 255, 255),
                 cv::FILLED,
                 cv::LINE_8
                 );
     }
 
-    cv::GaussianBlur(mask, mask, cv::Size(21, 21), 11);
+    cv::GaussianBlur(mask, mask, cv::Size(7, 7), 5);
+    cv::resize(mask, mask, cv::Size(img.rows, img.cols));
     cv::Mat blended;
-    alphaBlend(img, grayscale, 255 - mask, blended);
+    alphaBlend(img, grayscale, mask, blended);
 
     return blended;
 }
