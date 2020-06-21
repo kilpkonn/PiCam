e4d81b8b9a22ea9a985795fca261c2d5b960e102
tavo <tavo.annus@gmail.com>
2020-06-21 20:09:51 +0300

    Add -p option

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <iostream>
+#include <string>
 
 #include "cam/picam.h"
 
@@ -11,18 +12,31 @@ int main(int argc, char** argv) {
 
     bool blur = false;
     bool grayscale = false;
+    int port = 8080;
 
     for (int i = 1; i < argc; i++) {
         std::cout << "Arg " << i << ": " << argv[i] << std::endl;
         string arg(argv[i]);
-        if (arg == "-blur") {
+
+        if (arg == "-p" || arg == "--port") {
+            if (i < argc) {
+                port = std::stoi(argv[i + 1]);
+                i++;
+                // TODO: Error handling for strings
+            } else {
+                std::cout << "Please specify port number! Example: -p 8080" << std::endl;
+                return 1;
+            }
+        }
+
+        if (arg == "-b" || arg == "--blur") {
             blur = true;
-        } else if (arg == "-gray") {
+        } else if (arg == "-g" || arg == "--greyscale") {
             grayscale = true;
         }
     }
 
-    PiCam piCam(0, 8080);
+    PiCam piCam(0, port);
     piCam.setBlur(blur);
     piCam.setGrayscale(grayscale);
 
