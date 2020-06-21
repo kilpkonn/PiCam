a6666e2dd156999862ee8b112257deef71acad89
tavo <tavo.annus@gmail.com>
2020-06-21 10:47:41 +0300

    I\d say grayscale image is done for now

--- src/cam/graphics/graphics.h
+++ src/cam/graphics/graphics.h
@@ -10,7 +10,25 @@
 
 class Graphics {
 public:
+    /**
+     * Grayscale image background whilst keeping highlights colorful.
+     * Uses GaussianBlur for smoother edges, so that definitely has impact on performance.
+     *
+     * @param img - image to grayscale background, use BGR just in case
+     * @param highlights - cv::Rect objects that represent areas, which are kept colorful
+     * @param radius - extra radius around rectangles to keep colorful
+     * @return new cv::Mat image with grayscale background
+     */
     static cv::Mat grayscaleBackground(const cv::Mat& img, const std::vector<cv::Rect>& highlights, const int& radius);
+
+    /**
+     * Alpha blend 2 cv::Mat images
+     *
+     * @param img1 - image 1, aka where mask is 255
+     * @param img2 - image 2, aka where mask is 0
+     * @param mask - cv::Mat grayscale image with weights to use while blending
+     * @param blended - output image (result of blending)
+     */
     static void alphaBlend(const cv::Mat& img1, const cv::Mat& img2, const cv::Mat& mask, cv::Mat& blended);
 
 };
